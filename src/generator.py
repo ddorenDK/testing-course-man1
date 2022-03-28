616b0c640b32ba570f18c7edb32410a413b8d1c5
Dorin M <89907196+ddorenCorti@users.noreply.github.com>
2022-03-28 03:05:09 +0200

    paths update

--- src/generator.py
+++ src/generator.py
@@ -10,7 +10,7 @@ class Generator:
     # Input - Sample File Location
     # Output - Dictionary, ex. {'name': '?', 'surname': '?', 'gender': '?'}
     @staticmethod
-    def genFullNameAndGender(sampleFile = "../datasamples/person-names.json"):
+    def genFullNameAndGender(sampleFile = "datasamples\person-names.json"):
         with open(sampleFile, "r", encoding='UTF-16') as f:
             data = json.load(f)
             #Generate Random Number 
@@ -58,7 +58,7 @@ class Generator:
     # Input - sample file location
     # Output - String: address, ex: 'Leensgade'
     @staticmethod
-    def genStreet(sampleFile = "../datasamples/street-names.json"):
+    def genStreet(sampleFile = "datasamples\street-names.json"):
         with open(sampleFile, "r") as f:
             data = json.load(f)
             #Generate Random Number 
@@ -115,7 +115,7 @@ class Generator:
     # Input - Sample File Location
     # Output - Dictionary: ex. {'name': '?', 'code': '?'}
     @staticmethod
-    def genTownAndPostalCode(sampleFile = "../datasamples/addresses.json"):
+    def genTownAndPostalCode(sampleFile = "datasamples\addresses.json"):
          with open(sampleFile, "r") as f:
             data = json.load(f)
             #Generate Random Number, 
