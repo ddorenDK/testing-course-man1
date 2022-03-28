c792b966e690792ff82c1123a5b7e5e516a396b8
Dorin M <89907196+ddorenCorti@users.noreply.github.com>
2022-03-28 02:18:00 +0200

    toJson for person added, person creation in app.py added

--- src/app.py
+++ src/app.py
@@ -1,12 +1,39 @@
 import argparse
-
+import json
 from person import person
 
+#Variables
+outputPath = "../output/output.json"
+
+#Arguments
 parser = argparse.ArgumentParser(description = "Generates Fake information for non-existing danish persons")
-parser.add_argument("--instances", type = int, default = 5, help = "How many data instances to generate, value = int")
+parser.add_argument("--instances", type = int, default = 1, help = "How many data instances to generate, value = int")
 parser.add_argument("--datatype", type = str, default = "person",
   choices=['cpr', 'namegender', 'namegenderbirthdate', 'cprnamegender', 'cprnamegenderbirthdate', 'address', 'phone', 'person'], help = "What to generate" )
+args = parser.parse_args()
 
-yoperson = person()
-yoperson.printPerson()
+def main():
+  instances = args.instances
+  datatype = args.datatype
+
+  if datatype == 'person':
+    jsonString = '['
+    for it in range (1, instances+1):
+      tempPerson = person()
+      tempJsonString = tempPerson.toJson()
+      if it is not instances:
+        tempJsonString = tempJsonString + ','
+      jsonString = jsonString + tempJsonString
+    jsonString = jsonString + ']'
+    with open(outputPath, "w+") as file:
+      file.write(jsonString)
+
+
+
+main()
+
+
+
+# yoperson = person()
+# yoperson.printPerson()
 
--- src/person.py
+++ src/person.py
@@ -1,3 +1,5 @@
+import json
+from operator import ge
 from generator import Generator
 from address import address
 
@@ -9,7 +11,7 @@ class person:
         self.gender = Initials['gender']
         self.birthdate = Generator.genBirthDate()
         self.cpr = Generator.genCPR(self.gender, self.birthdate)
-        self.phone = Generator.genPhoneNumber
+        self.phone = Generator.genPhoneNumber()
         self.address = address()
 
     #Debug
@@ -17,3 +19,14 @@ class person:
         print(f'First Name: {self.firstName} \nLast Name: {self.lastName} \nGender: {self.gender} \nBirthdate: {self.birthdate} \nCPR: {self.cpr}')
         #Address
         print(f'Town: {self.address.town} \nZip Code: {self.address.zipCode} \nStreet Name and Number: {self.address.street} {self.address.number} \nFloor: {self.address.floor} \nDoor: {self.address.door}')
+
+    def toJson(self):
+        jsonString = ''
+        personString = f'"firstName":"{self.firstName}", "lastName":"{self.lastName}", "gender":"{self.gender}", "birthdate":"{self.birthdate}", "cpr":"{self.cpr}", "phone":"{self.phone}",'
+        personString = '{' + personString 
+        addressString = str(self.address.__dict__).replace('{','')
+        addressString = addressString.replace("'",'"')
+        jsonString = personString + addressString
+        return jsonString
+
+
