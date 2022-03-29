b8c5083ddcac798b43345c6c202cbab83830becf
Dorin M <89907196+ddorenCorti@users.noreply.github.com>
2022-03-29 11:09:36 +0200

    added tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,7 +3,7 @@ from src.generator import Generator
 
 #Unit Tests
 #TODO
-# ADD PROPER COMMENTS PLEASE
+# ADD PROPER COMMENTS
 
 def test_genFullNameAndGender():
     output = Generator.genFullNameAndGender()
@@ -37,9 +37,14 @@ def test_genStreetNumber():
     #Check if is numeric
     assert(output[:-1].isnumeric() or output.isnumeric())
 
-def test_genDoorNumber():
-    output = Generator.genDoorNumber()
-    assert(output.isnumeric())
+def test_genPhoneNumber():
+    output = Generator.genPhoneNumber()
+    allowedStarts = ('2','30','31','40','41','42','50','51','52','53','60','61','71','81','91','92','93','342','344','345','346','347','348','349','356','357','359','362','365','366','389','398','431','441','462','466','468','472','474','476','478','485','486','488','489','493','494','495','496','498','499','542','543','545','551','552','556','571','572','573','574','577','579','584','586','587','589','597','598','627','629','641','649','658','662','663','664','665','667','692','663','694','697','771','772','782','783','785','786','788','789','826','827','829')
+    assert(len(output) == 8)
+    assert(output.isnumeric)
+    assert(isinstance(output, str))
+    assert(output.startswith(allowedStarts))
+    
 
 
 
