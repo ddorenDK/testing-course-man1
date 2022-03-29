fd3570a41bd1c9b3c164695d5f1b793c648affe2
Dorin M <89907196+ddorenCorti@users.noreply.github.com>
2022-03-29 04:01:07 +0200

    added some tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,9 +1,44 @@
 import pytest
-
 from src.generator import Generator
 
-
+#Unit Tests
+#TODO
+# ADD PROPER COMMENTS PLEASE
 
 def test_genFullNameAndGender():
     output = Generator.genFullNameAndGender()
     assert isinstance(output['name'], str)
+    assert isinstance(output['surname'], str)
+    assert isinstance(output['gender'], str)
+    assert (output['gender'] == 'male' or output['gender'] == 'female')
+
+def test_genBirthDate():
+    output = Generator.genBirthDate()
+    assert (len(output) == 10)
+    #TODO
+    # Also check if date is valid
+
+def test_genCPR():
+    output = Generator.genCPR('male','12-12-1988')
+    assert(len(output) == 11)
+    #Test for males, last character is odd
+    assert(int(output[-1:]) % 2 != 0)
+    #Test for females, last character is even
+    output = Generator.genCPR('female','12-12-1988')
+    assert(int(output[-1:]) % 2 == 0)
+
+def test_getStreet():
+    output = Generator.genStreet()
+    #Check if alphabetical
+    assert(output.isalpha())
+
+def test_streetNumber():
+    output = Generator.genStreetNumber()
+    #Check if is numeric
+    assert(output[:-1].isnumeric() or output.isnumeric())
+
+
+
+
+
+
