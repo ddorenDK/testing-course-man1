077e148777555ceaf5d629f735719fb2e7bb79b8
Dorin M <89907196+ddorenCorti@users.noreply.github.com>
2022-03-29 11:27:58 +0200

    added address instance option

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 from person import person
+from address import address
 
 #Variables
 outputPath = "../output/output.json"
@@ -8,7 +9,7 @@ outputPath = "../output/output.json"
 #Arguments
 parser = argparse.ArgumentParser(description = "Generates Fake information for non-existing danish persons")
 parser.add_argument("--instances", type = int, default = 1, help = "How many data instances to generate, value = int")
-parser.add_argument("--datatype", type = str, default = "person",
+parser.add_argument("--datatype", type = str, default = "address",
   choices=['cpr', 'namegender', 'namegenderbirthdate', 'cprnamegender', 'cprnamegenderbirthdate', 'address', 'phone', 'person'], help = "What to generate" )
 args = parser.parse_args()
 
@@ -27,17 +28,32 @@ def main():
       tempJsonString = tempPerson.toJson()
       if it is not instances:
         tempJsonString = tempJsonString + ','
-      jsonString = jsonString + tempJsonString
-    jsonString = jsonString + ']'
+      jsonString += tempJsonString
+    jsonString += ']'
     with open(outputPath, "w+") as file:
       file.write(jsonString)
+
+  #TODO
+  #Extract Method
+  if datatype == 'address':
+    jsonString = '['
+    for it in range (1, instances+1):
+      tempAddress = address()
+      tempJsonString = str(tempAddress.__dict__).replace('{','')
+      tempJsonString = tempJsonString.replace("'",'"')
+      if it is not instances:
+        tempJsonString = tempJsonString + ','
+      jsonString += tempJsonString
+    jsonString += ']'
+    with open(outputPath, "w+") as file:
+      file.write(jsonString)
+      
+        
+
+
 
 
 
 main()
 
-
-
-# yoperson = person()
-# yoperson.printPerson()
 
